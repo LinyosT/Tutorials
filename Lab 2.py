945ecc7ba2899100544ef42db2e19c2ca4a88eba
Louis <louis.roberts.wlc@gmail.com>
2023-01-27 11:39:26 +0000

    Test

---
+++ Lab 2.py
@@ -0,0 +1,51 @@
+#3-1 Names
+
+# names = ["John", "Shelly", "Stefan", "Zach", "Andy"]
+
+# for name in names:
+#     print(f"Hello, {name}!")
+
+
+#3-8 Seeing the world
+
+# locations = ["Japan", "Chernobyl", "Murica", "Poland"]
+
+# print("Unsorted Locations:")
+# print(locations)
+# print("Alphabetically sorted locations:")
+# print(sorted(locations))
+# print("Reverse Alphabetically Sorted Locations")
+# print(sorted(locations, reverse=True))
+
+# print("Standard order:")
+# print(locations)
+# print("Reversed:")
+# locations.reverse()
+# print(locations)
+# print("Back to normal:")
+# locations.reverse()
+# print(locations)
+
+#3-10 Every Function
+
+# list = ["Metal Gear", "The Evil Within", "WoW", "FF XIV", "Half-Life"]
+
+# #length of a list
+# print(len(list))
+# #append to a list
+# print(list)
+# list.append("F.E.A.R")
+# print(list)
+# #insert into a list
+# list.insert(0, "Dead Space")
+# print(list)
+# #Delete from list
+# del list[0]
+# print(list)
+# #Delete from list (Again)
+# list.remove("WoW")
+# print(list)
+# #pop from list - remove an item from a list while passing on the value.
+# x = list.pop(0)
+# print(x)
+
